eaff1808707df6fdc79f67300c563bb129a46c69
Tausif <tausif@Tausifs-Mac-mini.local>
2025-10-20 16:45:01 +0600

    about and faq content updated

--- app/_components/about.jsx
+++ app/_components/about.jsx
@@ -8,7 +8,7 @@ const AboutSection = () => {
         <div className='grid md:grid-cols-2 grid-cols-1 md:gap-8 gap-4  items-center'>
           <div>
             <Image
-              src='/images/about.png'
+              src='/images/img-about.jpg'
               alt='About Thumbnail'
               width={640}
               height={500}
@@ -18,14 +18,29 @@ const AboutSection = () => {
             <h2 className='text-heading xl:text-5xl lg:text-4xl text-2xl  font-semibold'>
               About Nora Health
             </h2>
-            <p className='2xl:text-[24px] xl:text-[24px] lg:text-[20px] text-[14px] text-pg lg:my-7 my-3'>
-              At Nora Health, we believe contraception should be simple, safe,
-              and stigma-free. Our trusted online service connects patients with
-              qualified UK clinicians without the hassle of GP appointments or
-              long waits. Run by experienced pharmacists, our mission is to
-              enhance women’s health by offering free contraceptive pill
-              delivery to your door.
-            </p>
+            <div className='my-4'>
+              <p className='2xl:text-[24px] xl:text-[24px] lg:text-[20px] text-[14px] text-pg lg:mb-2 mb-1'>
+                At Norahealth, we believe contraception should be simple, safe,
+                and stigma-free.
+              </p>
+              <p className='2xl:text-[24px] xl:text-[24px] lg:text-[20px] text-[14px] text-pg lg:mb-2 mb-1'>
+                Our trusted online services connect patients with a clinical
+                team of experienced pharmacists and doctors — with over 15 years
+                of expertise in women's health. We know that everyone's
+                different. That's why our advice is{' '}
+                <span className='text-theme'>personal</span>,{' '}
+                <span className='text-theme'>inclusive</span>, and{' '}
+                <span className='text-theme'>culturally sensitive</span> — so
+                you feel heard, respected, and supported every step of the way.
+              </p>
+              <p className='2xl:text-[24px] xl:text-[24px] lg:text-[20px] text-[14px] text-pg lg:mb-2 mb-1'>
+                We will not be stopping there however - Nora Health plans to
+                expand to support women across every stage of life, from
+                contraception to menopause. Whether you're seeking advice or
+                treatment our mission is to empower you with compassionate,
+                accessible healthcare.
+              </p>
+            </div>
             <div className='grid grid-cols-2 md:gap-5 gap-4'>
               <CounterUpBox
                 num='480'
