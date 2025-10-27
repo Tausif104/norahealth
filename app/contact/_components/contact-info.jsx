7e235ea9602dbb5f0cd34b35d6df34d1087c7df5
Tausif <tausif@Tausifs-Mac-mini.local>
2025-10-27 15:42:46 +0600

    contact info done

---
+++ app/contact/_components/contact-info.jsx
@@ -0,0 +1,59 @@
+import Image from 'next/image'
+
+const ContactInfo = () => {
+  return (
+    <div className='bg-[#FFF4EF] py-[32px] px-[40px] rounded-[16px]'>
+      <h2 className='text-heading font-semibold text-[32px] mb-[30px]'>
+        Contact us
+      </h2>
+      <div>
+        <div className='flex items-center gap-[15px] border-b border-[#E9D4BB] pb-[20px] mb-[20px]'>
+          <Image
+            src='/images/icons/envelope.svg'
+            alt='email'
+            width={46}
+            height={46}
+          />
+
+          <div>
+            <h4 className='text-theme text-[18px] font-semibold'>Email</h4>
+            <p>
+              <a href='mailto:thepharmaclinic@gmail.com' className='text-pg'>
+                thepharmaclinic@gmail.com
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className='flex items-center gap-[15px]'>
+          <Image
+            src='/images/icons/phone.svg'
+            alt='email'
+            width={46}
+            height={46}
+          />
+
+          <div>
+            <h4 className='text-theme text-[18px] font-semibold'>Phone</h4>
+            <p>
+              <a href='tel:thepharmaclinic@gmail.com' className='text-pg'>
+                0208 679 7198
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className='mt-8'>
+        <Image
+          src='/images/contact.png'
+          width={490}
+          height={310}
+          alt='Contact Image'
+          className='w-full'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ContactInfo
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,20 @@
 import PageBanner from '@/components/global/page-banner'
+import ContactInfo from './_components/contact-info'
 
 const ContactPage = () => {
-  return <PageBanner title='Contact Us' />
+  return (
+    <>
+      <PageBanner title='Contact Us' />
+      <div className='container custom-container mx-auto section-padding'>
+        <div className='grid grid-cols-5 sm:px-0 px-[24px]'>
+          <div className='col-span-2'>
+            <ContactInfo />
+          </div>
+          <div className='col-span-3'>Get in touch</div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default ContactPage
